aabbaaca9c50cf12131597645d88b2c25e880508
andfroswe <gummianka88@gmail.com>
2015-07-28 12:20:22 +0200

    Allt klart, mainloop fungerar ej

--- matriskoll.py
+++ matriskoll.py
@@ -10,23 +10,12 @@ def kollaMatris(matris):
     resultat = kollaVer(matris)
     if resultat == True:
         return True
-    return False
-
-def kollaRad(vektor):
-    counter = 0
-    antal_i_rad = 5
-    for i in range(len(vektor)):
-        # Startklausul
-        if i == 0:
-            counter += 1
-            continue
-        if vektor[i] == vektor[i-1]:
-            counter += 1
-        else:
-            counter = 1
-        print counter
-        if counter == antal_i_rad:
-            return True
+    resultat = kollaDiagonalHoger(matris)
+    if resultat == True:
+        return True
+    resultat = kollaDiagonalVanster(matris)
+    if resultat == True:
+        return True
     return False
 
 def kollaHor(matris):
@@ -36,12 +25,32 @@ def kollaHor(matris):
             return True
     return False
 
+def kollaRad(vektor):
+    counter = 0
+    antal_i_rad = 5
+    tomTecken = [" "]
+    for i in range(len(vektor)):
+        # Startklausul
+        if i == 0:
+            counter += 1
+            continue
+        elif vektor[i] in tomTecken:
+            counter = 1
+            continue
+        elif vektor[i] == vektor[i-1]:
+            counter += 1
+        else:
+            counter = 1
+        if counter == antal_i_rad:
+            return True
+    return False
+
 def kollaVer(matris):
-    vertMatris = flippaMatris(matris)
+    vertMatris = raderTillKolumner(matris)
     resultat = kollaHor(vertMatris)
     return resultat
 
-def flippaMatris(matris):
+def raderTillKolumner(matris):
     flippadMatris = copy.deepcopy(matris)
     rader = len(matris)
     kolumner = len(matris)
@@ -49,7 +58,51 @@ def flippaMatris(matris):
         for j in range(kolumner):
             flippadMatris[j][i] = matris[i][j]
     return flippadMatris
+
+def kollaDiagonalHoger(matris):
+    """Kollar både höger och vänster diagonal"""
+    antal_i_rad = 5
+    rader = len(matris)
+    kolumner = len(matris[0])
+    maxRader = rader - (antal_i_rad - 1)
+    maxKolumner = kolumner - (antal_i_rad - 1)
+    # Kolla alla diagonaler som startar i kolumn 0 utom [0,0]
+    diagonalRader = range(maxRader)
+    diagonalRader.remove(0)
+    for i in diagonalRader:
+        diagonal = hamtaDiagonal(matris, rad = i)
+        resultat = kollaRad(diagonal)
+        if resultat == True:
+            return True
+    # Kolla alla diagonaler som startar i rad 0
+    for i in range(maxKolumner):
+        diagonal = hamtaDiagonal(matris, kolumn = i)
+        resultat = kollaRad(diagonal)
+        if resultat == True:
+            return True
+    return False
     
+def hamtaDiagonal(matris, rad = 0, kolumn = 0):
+    antal_i_rad = 5
+    rader = len(matris)
+    kolumner = len(matris[0])
+    vektor = []
+    while rad<=(rader-1) and kolumn<=(kolumner-1):
+        element = matris[rad][kolumn]
+        vektor.append(element)
+        rad += 1
+        kolumn += 1
+    return vektor
+
+def kollaDiagonalVanster(matris):
+    speglad = horisontellSpeglingMatris(matris)
+    resultat = kollaDiagonalHoger(speglad)
+    return resultat
+    
+def horisontellSpeglingMatris(matris):
+    speglad_temp = zip(*matris[::-1])
+    speglad = copy.deepcopy(speglad_temp)
+    return speglad
 
 def koraTest(test):
     """ Skriver ut snygga tester"""
@@ -63,21 +116,3 @@ def skrivaMatris(matris):
     print 'Skriver ut matris...'
     for row in matris:
         print row
-
-def main():
-    verTrue = [['x', 'x', '0', 4, 'h'],
-               ['x', 'c', 'f', 4, 'h'],
-               ['r', '4', 'f', 4, 'h'],
-               ['x', 'x', 'x', 'x', 'x'],
-               ['x', 'c', 'f', 4, 'h']]
-
-    falseMatrix = [['x', 'x', '0', 4, 'h'],
-                    ['x', 'c', 'f', 't', 'h'],
-                    ['x', '4', 'f', 4, 'h'],
-                    ['e', 'x', 'x', 'x', 'x'],
-                    ['x', 'c', 'f', 4, 'h']]
-
-
-    res = kollaMatris(verTrue)
-    print res
-main()
